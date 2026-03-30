d4e22553f7c1bcfd31c194ae63c39db21bef476a
Enrique Leonel Martínez <emartinez.03@hotmail.com>
2026-03-30 14:17:52 -0300

    chore: remove legacy mongoose models, tests, and utility scripts

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  verbose: true,
-  testPathIgnorePatterns: ['/node_modules/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['services/**/*.js', 'controllers/**/*.js'],
-};
--- test-prisma.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const p = require('./lib/prisma');
-p.$connect()
-    .then(() => { console.log('Prisma OK - conectado a Supabase'); p.$disconnect(); })
-    .catch(e => { console.error('Error:', e.message); process.exit(1); });
